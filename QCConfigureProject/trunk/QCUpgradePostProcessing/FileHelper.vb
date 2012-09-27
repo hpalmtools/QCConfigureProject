968ad8ee01fe83a39f7721cc880cb5948734555e
da-sheng <da-sheng@fbb83f78-0b98-4443-9e30-7517c0a9aef6>
2012-09-27 08:32:08 +0000

    Add CleanWorkflowCode Action. The action item likes: ALLSTD,CleanWorkflowCode,common.tds;defects.tds,\Requirements\Local Workflow Archive,Undefined, The action does: 1. Download the specified workflow codes, here are common.tds and defects.tds, from the ALM project 2. Archive the downloaded codes in a zip file 3. Delete the specified workflow codes from ALM project 4. Create or use the exisiting requirement, and save the zip file in it. In sample, the requirement name is 'Local Workflow Archive', its type is 'Undefined', and it is under the 'Requirements' folder.
    Add two more actions, DownloadWorkflowCode and DeleteWorkflowCode, for the help. Their action items like:
    ALLSTD,DownloadWorkflowCode,*.*,,,
    ALLSTD,DeleteWorkflowCode,*.*,,,

---
+++ QCConfigureProject/trunk/QCUpgradePostProcessing/FileHelper.vb
@@ -0,0 +1,43 @@
+﻿Imports System.IO
+Imports System.IO.Packaging
+
+Public Class FileHelper
+    Const PackageRelationshipType As String = "http://schemas.microsoft.com/opc/2006/sample/document"
+    Public Shared Sub ClearContentOfFile(ByVal filePath As String)
+        System.IO.File.WriteAllText(filePath, "")
+    End Sub
+
+    Public Shared Sub CreateZipPackageWithFilesOfDirectory(ByVal zipPackagePath As String, ByVal directory As String)
+        Dim files() As String = System.IO.Directory.GetFiles(directory)
+        CreateZipPackage(zipPackagePath, files)
+    End Sub
+
+    Public Shared Sub CreateZipPackage(ByVal zipPackagePath As String, ByVal files() As String)
+        Using package As Package = package.Open(zipPackagePath, FileMode.Create)
+            For Each filePath As String In files
+                Dim packagePartPath = Path.GetFileName(filePath)
+                AddFileToPackage(filePath, packagePartPath, package)
+            Next
+        End Using
+    End Sub
+
+    Shared Sub AddFileToPackage(ByVal filePath As String, ByVal packagePartPath As String, ByVal package As Package)
+        Dim partUri As Uri = PackUriHelper.CreatePartUri(New Uri(packagePartPath, UriKind.Relative))
+        Dim packagePart As PackagePart = package.CreatePart(partUri, "", CompressionOption.Normal)
+        Using fileStream As New FileStream(filePath, FileMode.Open, FileAccess.Read)
+            CopyStream(fileStream, packagePart.GetStream())
+        End Using
+        package.CreateRelationship(packagePart.Uri, TargetMode.Internal, PackageRelationshipType)
+    End Sub
+
+    Shared Sub CopyStream(ByVal source As Stream, ByVal target As Stream)
+        Const bufSize As Integer = &H1000
+        Dim buf(bufSize - 1) As Byte
+        Dim bytesRead As Integer = 0
+        bytesRead = source.Read(buf, 0, bufSize)
+        Do While bytesRead > 0
+            target.Write(buf, 0, bytesRead)
+            bytesRead = source.Read(buf, 0, bufSize)
+        Loop
+    End Sub
+End Class
